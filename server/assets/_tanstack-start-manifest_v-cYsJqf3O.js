c2213619e55a8ff74434091118345bb5c93220f7
Fernando Lavoura <fernando.lavoura@autojun.com.br>
2026-05-16 23:08:43 -0300

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-cYsJqf3O.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/lucianov/OneDrive - BADESC/Documentos/fernando/capacitação/studyflow-landing-page/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/studyflow-landing-page/assets/index-SaDdn_fJ.js"] }, "/": { filePath: "C:/Users/lucianov/OneDrive - BADESC/Documentos/fernando/capacitação/studyflow-landing-page/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/studyflow-landing-page/assets/index-BRN7HVim.js"] } }, clientEntry: "/studyflow-landing-page/assets/index-SaDdn_fJ.js" });
+export {
+  tsrStartManifest
+};
---
+++ server/index.js
@@ -0,0 +1,9 @@
+import { _, $ } from "./assets/worker-entry-7g3vR1yg.js";
+import "node:events";
+import "node:async_hooks";
+import "node:stream/web";
+import "node:stream";
+export {
+  _ as createServerEntry,
+  $ as default
+};
